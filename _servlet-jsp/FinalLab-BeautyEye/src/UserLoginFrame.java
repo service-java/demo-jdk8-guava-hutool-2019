6079352b148caa68b319081e264ed3bab053995a
luo0412 <1095847440@qq.com>
2019-04-29 10:43:16 +0800

    small change

--- _servlet-jsp/FinalLab-BeautyEye/src/UserLoginFrame.java
+++
@@ -1,8 +0,0 @@
-import components.LoginDialog;
-
-public class UserLoginFrame {
-    public static void main(String[] args) {
-        LoginDialog loginDlg = new LoginDialog();
-        loginDlg.setVisible(true);
-    }
-}
