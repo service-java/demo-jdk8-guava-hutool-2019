8a652ea5b26799707ae04b45a24230b3bcdf5291
luo0412 <1095847440@qq.com>
2019-06-26 17:04:55 +0800

    small

--- __algorithm/java-interview/Java8Interface.java
+++
@@ -1,7 +0,0 @@
-package com.wiley.javainterviewsexposed.chapter08;
-
-public interface Java8Interface {
-    void alpha();
-    int beta default { return 6; };
-    String omega final { return "Cannot override"; };
-}
--- __algorithm/java-interview/Square.java
+++
@@ -1,8 +0,0 @@
-package com.wiley.javainterviewsexposed.chapter08;
-
-public class Square extends Rectangle {
-
-    public Square(final int sideLength) {
-        super(sideLength, sideLength);
-    }
-}
